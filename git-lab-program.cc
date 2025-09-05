09ceca67e648755543132a41a0dbdebfb8304912
Harrison <hb586424@ohio.edu>
2025-09-05 15:36:06 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Harrison Bond
+ *        Date: 9/5/2025
+ * Description: Outputs a message to the user
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
